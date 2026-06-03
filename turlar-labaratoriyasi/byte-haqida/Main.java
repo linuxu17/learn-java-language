e98dfbce7ce5ad357cf7384f4f2f035dc22edd46
linuxu17 <linuxu177@gmail.com>
2026-06-03 21:39:16 +0500

    byte-haqida

---
+++ turlar-labaratoriyasi/byte-haqida/Main.java
@@ -0,0 +1,21 @@
+/*
+    -Byte tipi java dasturlash tilida eng kichik sonlarni ifoda etadigan primitiv tur xisoblanadi
+    -Uning hajmi 1bayt va qiymatlar oralig'i -128 dan 127 gacha jami 255 da qiymatni qabul qiladi
+    -Agar qiymat oraliqdagi sonlardan katta yoki kichik bo'lsa overflow xatosi chiqaradi
+
+    Asosiy metodlari
+
+    1) Byte.parseByte("12"); stringdan baytga transfer
+    2) Byte.toString(17); baytdan string tipiga o'tish
+    3) Byte.compare(a, b); solishtirish
+    5) Byte.MAX_VALUE baytdagi eng katta qiymat 127
+    6) Byte.MIN_VALUE baytdagi eng kichik qiymat -128
+
+*/
+
+public class Main{
+
+public static void main(String[] args){
+
+    }
+}
