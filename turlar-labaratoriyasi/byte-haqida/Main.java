2019490c8f3793104e8e0cdc9dc1ce6f54afa1ab
linuxu17 <linuxu177@gmail.com>
2026-06-06 12:08:05 +0500

    Byte tipini o'zlashtirdik va standart metodlarni sinab ko'rdik

--- turlar-labaratoriyasi/byte-haqida/Main.java
+++ turlar-labaratoriyasi/byte-haqida/Main.java
@@ -4,28 +4,19 @@
     -xotira xajmi 1bayt
     -qiymatlar oralig'i -128 dan 127 gacha
     -Byte nomli wrapper sinfi mavjud
-    -Byte sinfinig standart metodlari 21ta
 
-    KONVERTATSIYA METODLARI
-
-    1)byteValue() - obekting 10lik tizimdagi byte qiymatini ajratib oladi
-    2)shortValue() - obyetni shot tipiga kengaytiradi
-    3)intValue() - obektni int tipiga kengaytiradi
-    4)longvalue() - obektni long tipiga kengaytiradi
-    5)floatValue() - obektni float tipiga kengaytiradi
-    6)doubleValue() - obektni double tipiga kengaytiradi
-
-    PARSING METODLARI
-
-    7)parseByte(String s) - satrni 10lik tizim asosida bytega o'giradi
-    8)parseByte(String s, int x) - s satrni x sanoq tizimi asosida bytega  o'giradi
-    9)decode(String s) - 16lik (0x) yoki 8lik(0) formatidagi satrlarni Byte wrapper obektiga aylantiradi
-
-    OBEKTGA O'RASH WRAPPER OBEKTGA AYLANTIRISH
-
-    10)valueOf(byte a) - a ni Byte obektiga o'giradi
-    11)valueOf(String s) - s satrni Byte obektiga o'giradi
-    12)valueOf(String s, int x) s satrni x saniq tizimi asodisa Byte obektiga aylantiradi
+    STANDART METODLARI
+    byteValue()
+    shortValue()
+    intValue()
+    floatValue()
+    doubleValue()
+    longValue()
+    toString()
+    pareByte(String s)
+    valueOf()
+    compare(byte a, byte b)
+    decode(String s)
 
 
 */
@@ -34,44 +25,25 @@ public class Main{
 
 public static void main(String[] args){
 
-    Byte byte_ = 77;
+    byte son1 = 12;
+    Byte son2 = 15;
 
-    // Konvertatsiya
-    byte byte_qiymat = byte_.byteValue();
-    short short_qiymat = byte_.shortValue();
-    int int_qiymat = byte_.intValue();
-    long long_qiymat = byte_.longValue();
-    float float_qiymat = byte_.floatValue();
-    double double_qiymat = byte_.doubleValue();
+    //System.out.printlon(son1.toString()); // xato, kompilatsiya bo'lmaydi
+    System.out.println(son2.toString()); //to'g'ri, barcha metodlar wrapper sinfiga tegishli;
 
-    System.out.println("byte qiymat: " + byte_qiymat);
-    System.out.println("short qiymat: " + short_qiymat);
-    System.out.println("int qiymat: " + int_qiymat);
-    System.out.println("long qiymat: " + long_qiymat);
-    System.out.println("float qiymat: " + float_qiymat);
-    System.out.println("double qiymat: " + double_qiymat);
+    System.out.println(son2.byteValue()); // Byte obyektining byte qiymatini qaytaradi
+    System.out.println(son2.intValue()); // Byte obyektini int ga kengaytiradi int qaytaradi
+    System.out.println(Byte.parseByte("77")); // String qiymatni Byte obektiga o'girishga urinadi
+    System.out.println(Byte.parseByte("77", 8)); // String qiymatni berilgan son tiizmi bo'yicha Byte obektiga aylantirishga urinadi
+    //Yani paresByte(String s, int x) biror x saniq tizimiga tegishli sonni ifoda etuvchi s matnni Byte.MAX_VALUE dan oshib ketmasa uni Byte obektiga aylantirib byte qiymatini qaytaradi
+    System.out.println(Byte.parseByte("0A", 16)); // natija 10 chiqadi. 16 lik tizimdagi 0A soni 10lik tizimda 10ga teng
 
-    // Parsing
-    String byte_satr = "55";
+    System.out.println(Byte.decode("0x0B")); // 0x formatidagi string qiymatni Byte obektiga aylantirib byte qiymatini qaytaradi.
+    System.out.println(Byte.valueOf(son1)); // byte qiymatini Byte obektiga aylantiradi
 
-    byte str_dan_bytega = Byte.parseByte(byte_satr);
-    byte str_dan_bytega_2 = Byte.parseByte(byte_satr, 16);
-    byte str_dan_bytega_3 = Byte.decode("0x0F");
-    byte str_dan_bytega_4 = Byte.decode("013");
+    byte qiymat1 = 7, qiymat2 = 10;
 
-    System.out.println("Strdan bytega 1: " + str_dan_bytega);
-    System.out.println("Strdan bytega 2: " + str_dan_bytega_2);
-    System.out.println("Strdan bytega 3: " + str_dan_bytega_3);
-    System.out.println("Strdan bytega 4: " + str_dan_bytega_4);
+    System.out.println(Byte.compare(qiymat1, qiymat2)); // ikkita byte qiymat orasidagi farqni qaytaradi birinchi qiymatdan ikkinchi qiymatni ayiradi.
 
-    //WRAPPER OBEKTGA AYLANTIRISH
-    String byte_satr_2 = "0F";
-    byte wrapper_obyekt = Byte.valueOf((byte)byte_qiymat);
-    byte wrapper_obyekt_2 = Byte.valueOf(byte_satr);
-    byte wrapper_obyekt_3 = Byte.valueOf(byte_satr_2, 16);
-
-    System.out.println("bytedan bytega 1: " + wrapper_obyekt);
-    System.out.println("bytedan bytega 2: " + wrapper_obyekt_2);
-    System.out.println("bytedan bytega 3: " + wrapper_obyekt_3);
     }
 }
