ab0b376aadcb8e63d662e7352d6dec952add405d
linuxu17 <linuxu177@gmail.com>
2026-06-08 19:25:22 +0500

    Integer sinfida yangi metodlarni ko'rib chiqdik, jumladan Integer obektini qiymatini turli boshqa primitiv turlarga o'tkazib qiymatini qaytaruvchi metodlar byteValue, shortValue,intValue, floatValue, doubelValue va longValue

--- turlar-labaratoriyasi/int-haqida/Main.java
+++ turlar-labaratoriyasi/int-haqida/Main.java
@@ -11,7 +11,13 @@
     MAX_VALUE
     TYPE
     BYTES
-    
+
+    byteValue()
+    shortValue()
+    intValue()
+    floatValue()
+    doubleValue()
+    longValue()
 */
 
 
@@ -20,11 +26,19 @@ public class Main{
 
     public static void main(String[] args){
 
+        Integer int_son = Integer.valueOf("77"); // Integer wrapper sinfiga tegishli obekt yaratdik
+
         System.out.println(Integer.MIN_VALUE);// konstanta, eng kichik qiymatni qaytaradi
         System.out.println(Integer.MAX_VALUE);// konstanta, eng katta qiymatini qaytaradi
         System.out.println(Integer.TYPE); // konstanta, sinfning primitiv turini nomini qaytaradi: int
         System.out.println(Integer.BYTES); // konstanta, tipning bytelardagi xotira xajmini qaytaradi: 4
 
+        System.out.println(int_son.byteValue() + "-byte tipida"); // Integer obektining qiymatini bytega toraytiradi va qiymatni byteda qaytaradi
+        System.out.println(int_son.shortValue() + "-short tipida"); // Integer obektini qiymatini short tipiga toraytirb shortdagi qiymatni qaytaradi
+        System.out.println(int_son.intValue() + "-int tipida"); // Integer obektini qiymatini int primitiv turga o'zgartirib int tipida qiymatini qaytaradi
+        System.out.println(int_son.floatValue() + "-float tipida"); //Integer obektini qiymatini floatga kengayririb qiymatini float tipida qaytaradi
+        System.out.println(int_son.doubleValue() + "-double tipida"); //Integer obektini qiymatini doublega kengaytirib qiymatini doubel tipida qaytaradi
+        System.out.println(int_son.longValue() + "-long tipida"); //Integer obektini qiymatini long tipiga kengaytirib qiymatini long tipida qaytaradi
 
     }
 }
